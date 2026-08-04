608c227af15cbfcf981985d3c2dcb6b425acbf6a
Suresh Suri <psureshkumar@iitbhilai.ac.in>
2026-08-05 00:43:58 +0530

    change pwd bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import Locations from './modules/masters/Locations';
 import Trainings from './modules/trainings/Trainings';
 import Participants from './modules/participants/Participants';
 // import PublicReport from './modules/reports/PublicReport';
-import ChangePassword from './modules/change password/changePassword';
+import ChangePassword from './modules/change_password/ChangePassword';
 import Development from './modules/testdev/development';
 function App() {
   return (
